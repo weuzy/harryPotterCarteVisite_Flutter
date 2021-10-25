702fce1c79124186c2072cee5fa856449d07703b
#19weuzy <diopp1017@gmail.com>
2021-10-25 11:52:13 +0000

    mise en forme du texte

--- lib/Screens/visit_card.dart
+++ lib/Screens/visit_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class VisitCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,36 +13,51 @@ class VisitCard extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: EdgeInsets.all(15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[CircleAvatar(
-            radius: 70.0,
-            backgroundImage: AssetImage('assets/portrait.jpeg'),
-            ),
-            SizedBox(height: 10.0,),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Card(
-                child: Text('Harry Potter',
-                 style: TextStyle(fontFamily: 'JosefinSans'),
-                 ),
-              ),
-            ),
-            Card(
-                margin: EdgeInsets.only(top:30.0, bottom:15.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Il porte des lunettes rondes à monture noire et une fine cicatrice en forme éclair sur le front, souvent cachée par ses mèches. Harry est assez timide et plutôt modeste.',
-                   style: TextStyle(
-                     fontFamily: 'JosefinSans',
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 70.0,
+                  backgroundImage: AssetImage('assets/portrait.jpeg'),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Card(
+                    color: Colors.transparent,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Harry Potter',
+                        style: TextStyle(
+                            fontFamily: 'JosefinSans',
+                            fontSize: 30.0,
+                            height: 1.5,
+                            color: Colors.white,
+                          ),
+                      ),
+                    ),
                   ),
-              ),
-                )),
-          ]
-          ),
-        ), 
+                Card(
+                    margin: EdgeInsets.only(top: 30.0, bottom: 15.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Il porte des lunettes rondes à monture noire et une fine cicatrice en forme éclair sur le front, souvent cachée par ses mèches. Harry est assez timide et plutôt modeste.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'JosefinSans',
+                          fontSize: 20.0,
+                          height: 1.5,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    color: Colors.transparent,
+                ),
+              ]),
         ),
+      ),
     );
   }
 }
